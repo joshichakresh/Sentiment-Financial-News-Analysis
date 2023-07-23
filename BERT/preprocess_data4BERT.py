58d9f631ae4270fbc480356a853791242dd97282
joshichakresh <88558199+joshichakresh@users.noreply.github.com>
2023-07-24 00:07:09 +0530

    Update preprocess_data4BERT.py

--- BERT/preprocess_data4BERT.py
+++ BERT/preprocess_data4BERT.py
@@ -2,18 +2,18 @@ import torch
 import pandas as pd
 from transformers import BertTokenizer
 
-# Load the dataset without column headers
+# Loading the dataset without column headers
 data = pd.read_csv("all-data.csv", header=None, encoding='latin-1', names=['Sentiment', 'News Headline'])
 
-# Initialize the BERT tokenizer
+# Initializing the BERT tokenizer
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 
-# Tokenize and preprocess the text
+# Tokenizing and preprocessing the text
 input_ids = []
 attention_masks = []
 
 for text in data['News Headline']:
-    # Tokenize the text and add special tokens
+    # Tokenizing the text and adding special tokens
     encoded_text = tokenizer.encode_plus(
         text,
         add_special_tokens=True,
@@ -24,18 +24,18 @@ for text in data['News Headline']:
         return_tensors='pt'
     )
     
-    # Extract input IDs and attention mask
+    # Extracting input IDs and attention mask
     input_id = encoded_text['input_ids'].flatten()
     attn_mask = encoded_text['attention_mask'].flatten()
     
     input_ids.append(input_id)
     attention_masks.append(attn_mask)
 
-# Convert lists to tensors
+# Converting lists to tensors
 input_ids = torch.stack(input_ids)
 attention_masks = torch.stack(attention_masks)
 
-# Store the preprocessed dataset
+# Storing the preprocessed dataset
 preprocessed_data = pd.DataFrame({
     'Input IDs': input_ids.tolist(),
     'Attention Masks': attention_masks.tolist(),
