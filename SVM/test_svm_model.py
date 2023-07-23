193933e4a5c25a50477dca8331c3fb9fa65dde9f
joshichakresh <88558199+joshichakresh@users.noreply.github.com>
2023-07-24 00:09:49 +0530

    Update test_svm_model.py

--- SVM/test_svm_model.py
+++ SVM/test_svm_model.py
@@ -3,10 +3,10 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import SVC
 import joblib
 
-# Load the preprocessed dataset
+# Loading the preprocessed dataset
 data = pd.read_csv('preprocessed_data.csv')
 
-# Split the data into features (X) and target variable (y)
+# Splitting the data into features (X) and target variable (y)
 X = data['News Headline']
 y = data['Sentiment']
 
@@ -14,10 +14,10 @@ y = data['Sentiment']
 vectorizer = TfidfVectorizer()
 X_features = vectorizer.fit_transform(X)
 
-# Load the trained model
+# Loading the trained model
 loaded_model = joblib.load('saved_model.pkl')
 
-# Predict sentiment for a new news headline
+# Predicting sentiment for a new news headline
 new_headline = input("Enter a new news headline: ")
 new_feature = vectorizer.transform([new_headline])
 prediction = loaded_model.predict(new_feature)
